59446b488183f8df02c3908cd4c3a156dc97d610
urrburu <bbs4614@naver.com>
2021-02-16 12:48:04 +0900

    member signup form submit validation

--- src/main/java/com/studyolle/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/studyolle/account/AccountController.java
@@ -1,14 +1,42 @@
 package com.studyolle.studyolle.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.Errors;
+
+import javax.validation.Valid;
+
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
-        model.addAttribute("signUpForm", new SignUpForm());
+        //model.addAttribute("signUpForm", new SignUpForm());
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
+
+    @PostMapping("/sign-up")
+    public String signupSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        return "redirect:/";
+    }
+
 }
+
---
+++ src/main/java/com/studyolle/studyolle/account/SignUpFormValidator.java
@@ -0,0 +1,36 @@
+package com.studyolle.studyolle.account;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+/*
+    public SignUpFormValidator(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+ */
+    @Override
+    public boolean supports(Class<?> aClass){
+        return aClass.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors){
+        SignUpForm signUpForm = (SignUpForm)errors;
+        if(accountRepository.existsByEmail(signUpForm.getEmail())){
+            errors.rejectValue("email","invalid.email",new Object[]{signUpForm.getEmail()}, "already using email");
+        }
+        if(accountRepository.existsByNickname(signUpForm.getNickname())){
+            errors.rejectValue("nickname","invalid.nickname",new Object[]{signUpForm.getNickname()}, "already using nickname");
+        }
+    }
+
+}
