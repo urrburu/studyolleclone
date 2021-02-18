05568e7d979c5b2e433f88d4fa49f0af87ff3171
urrburu <bbs4614@naver.com>
2021-02-18 12:25:44 +0900

    member signup refactoring and test

--- src/main/java/com/studyolle/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/studyolle/account/AccountController.java
@@ -19,8 +19,7 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AccountController {
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder){
@@ -41,24 +40,10 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword())//TODO encoding 해야함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-        Account newAccount = accountRepository.save(account);
+        accountService.processNewAccount(signUpForm);
 
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setSubject("study olle, member join certification");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email="+newAccount.getEmail());
-        javaMailSender.send(mailMessage);
 
         return "redirect:/";
     }
-
 }
 
--- src/test/java/com/studyolle/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/studyolle/account/AccountControllerTest.java
@@ -5,10 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -16,6 +25,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AccountControllerTest {
     @Autowired private MockMvc mockMvc;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @MockBean
+    JavaMailSender javaMailSender;
 
     @DisplayName("sign-up display test")
     @Test
@@ -25,5 +40,20 @@ class AccountControllerTest {
                 .andExpect(view().name("account/sign-up"))
                 .andExpect(model().attributeExists("signUpForm"));
     }
+    @DisplayName("member join operation - input error")
+    @Test
+    void signUpSubmit_with_right_input() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .with(csrf())
+                .param("nickname", "chanhwi")
+                .param("email", "bbs4614@naver.com")
+                .param("password", "12345678"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
 
+        assertTrue(accountRepository.existsByEmail("bbs4614@naver.com"));
+
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
+
+    }
 }
