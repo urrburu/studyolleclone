2e816bddd06e30f32574195d7907879da4bf6a06
urrburu <bbs4614@naver.com>
2021-03-26 17:02:04 +0900

    add user login test

--- src/main/java/com/studyolle/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/studyolle/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
---
+++ src/test/java/com/studyolle/studyolle/main/MainControllerTest.java
@@ -0,0 +1,92 @@
+package com.studyolle.studyolle.main;
+
+import com.studyolle.studyolle.account.AccountRepository;
+import com.studyolle.studyolle.account.AccountService;
+import com.studyolle.studyolle.account.SignUpForm;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+    @Autowired MockMvc mockMvc;
+    @Autowired AccountService accountService;
+    @Autowired
+    AccountRepository accountRepository;
+
+    @BeforeEach
+    void beforeEach(){
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("keesun");
+        signUpForm.setEmail("keesun@email.com");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+
+    }
+    @AfterEach
+    void afterEach(){
+        accountRepository.deleteAll();
+    }
+    @DisplayName("Login success using Email")
+    @Test
+    void login_with_email() throws Exception{
+
+
+        mockMvc.perform(post("/login")
+                .param("username", "keesun@email.com")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("keesun"));
+
+    }
+
+
+    @DisplayName("Login success using Nickname")
+    @Test
+    void login_with_nickname() throws Exception{
+
+        mockMvc.perform(post("/login")
+                .param("username", "keesun")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("keesun"));
+
+    }
+    @DisplayName("Login Fail")
+    @Test
+    void login_fail() throws Exception{
+        mockMvc.perform(post("/login")
+                .param("username", "1111111")
+                .param("password", "000000000")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(unauthenticated());
+    }
+    @DisplayName("LOG OUT")
+    @Test
+    void logout() throws Exception{
+        mockMvc.perform(post("/logout")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
+
+    }
+
+
+}
