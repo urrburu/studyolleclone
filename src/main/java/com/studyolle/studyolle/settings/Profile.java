8c4276482b9bc874f654a2b0cfe3f2623c1e66f0
urrburu <bbs4614@naver.com>
2021-03-31 07:23:24 +0900

    make profile detail

---
+++ src/main/java/com/studyolle/studyolle/settings/Profile.java
@@ -0,0 +1,24 @@
+package com.studyolle.studyolle.settings;
+
+import com.studyolle.studyolle.domain.Account;
+import lombok.Data;
+
+@Data
+public class Profile {
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    public Profile(Account account){
+        this.bio = account.getBio();
+        this.url = account.getUrl();
+        this.occupation = account.getOccupation();
+        this.location = account.getLocation();
+
+    }
+}
