0167f16e91e677b8fe2cdbf680e520a8000afef4
JeongChanHwi <bbs4614@naver.com>
2021-07-22 15:57:38 +0900

    make notification service with querydsl but it has bug

--- src/main/java/com/studyolle/studyolle/modules/study/StudyService.java
+++ src/main/java/com/studyolle/studyolle/modules/study/StudyService.java
@@ -27,7 +27,6 @@ public class StudyService {
     public Study createNewStudy(Study study, Account account) {
         Study newStudy = repository.save(study);
         newStudy.addManager(account);
-        eventPublisher.publishEvent(new StudyCreatedEvent(newStudy));
         return newStudy;
     }
 
@@ -102,7 +101,7 @@ public class StudyService {
 
     public void publish(Study study) {
         study.publish();
-        //this.eventPublisher.publishEvent(new StudyCreatedEvent(study));
+        this.eventPublisher.publishEvent(new StudyCreatedEvent(study));
     }
 
     public void close(Study study) {
