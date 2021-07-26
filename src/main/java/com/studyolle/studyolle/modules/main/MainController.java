b48b439cf8204bdeb6d04d7bf2654f9e5a1910cf
JeongChanHwi <bbs4614@naver.com>
2021-07-26 18:23:57 +0900

    make notification call

--- src/main/java/com/studyolle/studyolle/modules/main/MainController.java
+++ src/main/java/com/studyolle/studyolle/modules/main/MainController.java
@@ -2,18 +2,28 @@ package com.studyolle.studyolle.modules.main;
 
 import com.studyolle.studyolle.modules.account.CurrentUser;
 import com.studyolle.studyolle.modules.account.Account;
+import com.studyolle.studyolle.modules.notification.NotificationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final NotificationRepository notificationRepository;
+
+
     @GetMapping("/")
     public String home(@CurrentUser Account account, Model model)
     {
         if(account != null){
             model.addAttribute(account);
         }
+
+        long count = notificationRepository.countByAccountAndChecked(account,false);
+        model.addAttribute("hasNotification", count>0);
         return "index";
     }
 
--- src/main/java/com/studyolle/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyolle/studyolle/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.studyolle.studyolle.modules.notification;
 
+import com.studyolle.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
